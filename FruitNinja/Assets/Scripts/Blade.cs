f8f873e5d011437a8657c9e8f8b965b89b18585b
Balaita Cosmin-Neculai <126869724+CosminBalaita2003@users.noreply.github.com>
2024-06-16 13:27:21 +0300

    Update Blade.cs

--- FruitNinja/Assets/Scripts/Blade.cs
+++ FruitNinja/Assets/Scripts/Blade.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Blade : MonoBehaviour
 {
+    public static Blade Instance { get; private set; } 
+
     public float sliceForce = 5f;
     public float minSliceVelocity = 0.01f;
 
@@ -17,9 +19,18 @@ public class Blade : MonoBehaviour
 
     private void Awake()
     {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);  
+            return;
+        }
+        Instance = this;
+        
         mainCamera = Camera.main;
         sliceCollider = GetComponent<Collider>();
         sliceTrail = GetComponentInChildren<TrailRenderer>();
+
+        DontDestroyOnLoad(gameObject);
     }
 
     private void OnEnable()
@@ -34,14 +45,26 @@ public class Blade : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0)) {
-            StartSlice();
-        } else if (Input.GetMouseButtonUp(0)) {
-            StopSlice();
-        } else if (slicing) {
-            ContinueSlice();
+        if (mainCamera == null)
+            mainCamera = Camera.main;
+
+        if (mainCamera != null)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                StartSlice();
+            }
+            else if (Input.GetMouseButtonUp(0))
+            {
+                StopSlice();
+            }
+            else if (slicing)
+            {
+                ContinueSlice();
+            }
         }
     }
+
 
     private void StartSlice()
     {
@@ -58,12 +81,26 @@ public class Blade : MonoBehaviour
     private void StopSlice()
     {
         slicing = false;
-        sliceCollider.enabled = false;
-        sliceTrail.enabled = false;
+
+        if (sliceCollider != null)
+            sliceCollider.enabled = false;
+        
+        if (sliceTrail != null)
+        {
+            sliceTrail.enabled = false;
+            sliceTrail.Clear();
+        }
     }
+
 
     private void ContinueSlice()
     {
+        if (mainCamera == null)
+        {
+            mainCamera = Camera.main;
+            if (mainCamera == null) return; 
+        }
+
         Vector3 newPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
         newPosition.z = 0f;
         direction = newPosition - transform.position;
@@ -73,10 +110,10 @@ public class Blade : MonoBehaviour
 
         transform.position = newPosition;
     }
-     public void SetTrailColor(Color color)
+
+    public void SetTrailColor(Color color)
     {
         sliceTrail.startColor = color;
         sliceTrail.endColor = color;
     }
-
 }
